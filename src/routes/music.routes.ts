de5d2760921c03110c193b79542a436db00d3ac2
welton <weltonsantosfr@gmail.com>
2024-01-04 17:33:21 -0300

    add music delete route

--- src/routes/music.routes.ts
+++ src/routes/music.routes.ts
@@ -3,6 +3,7 @@ import musicCreateController from "../controllers/music/musicCreate.controller";
 import tokenAuthMiddleware from "../middlewares/tokenAuth.middleware";
 import musicListController from "../controllers/music/musicList.controller";
 import musicUpdadeController from "../controllers/music/musicUpdade.controller";
+import musicDeleteController from "../controllers/music/musicDelete.controller";
 
 
 const routes = Router();
@@ -11,7 +12,7 @@ export const musicRoutes = () => {
   routes.post("/", tokenAuthMiddleware, musicCreateController);
   routes.get("/", musicListController);
   routes.patch("/", tokenAuthMiddleware, musicUpdadeController);
-  routes.delete("/", );
+  routes.delete("/", tokenAuthMiddleware, musicDeleteController);
   routes.post("/login", );
 
   return routes;
